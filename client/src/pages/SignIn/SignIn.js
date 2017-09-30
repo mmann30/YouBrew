42c301cfe8caa16b0c6b5ee8735f36d6348ae202
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-09-30 12:00:21 -0400

    initial commit

---
+++ client/src/pages/SignIn/SignIn.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+
+class SignIn extends Component {
+  render() {
+    <Container fluid>
+      <Row>
+        <Col size="md-6">
+
+        </Col>
+        <Col size="md-4">
+          
+        </Col>
+      </Row>
+    </Container>
+  }
+}
