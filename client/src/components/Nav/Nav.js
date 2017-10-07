5c2fa7464b490e6868f2c08e9e80a0af8d718ab5
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-10-07 14:05:08 -0400

    Availability page layout, Navbar styling

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Nav = () =>
-  <nav className="navbar navbar-default navbar-fixed-top">
+  <nav className="navbar navbar-default navbar-top">
     <div className="navbar-header">
       <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span className="sr-only">Toggle navigation</span>
--- client/src/pages/Availability.js
+++ client/src/pages/Availability.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
+import Nav from "../components/Nav";
+
 
 class Availability extends Component {
   render() {
     return (
-
       <Container fluid>
         <Row>
-          <Col size="md-6">
-
-              
-
-            <img id="youbrewassets" className="img-responsive" src="../assets/images/youbrewassets.png" />
-            <div className="assetsimage">
-            </div>
+          <Col size="md-9">
+            <div id="availability">
+          		<div className="panel panel-warning">
+          			<div className="panel-heading">
+          				<h3 className="panel-title">Avalable Brews</h3>
+          				{/* <span className="pull-right clickable"><i className="glyphicon glyphicon-chevron-up"></i></span> */}
+          			</div>
+          			<div className="panel-body">Beer XYZ
+          			  <label>
+                    {/* <input type="checkbox" value="Option 1"></input> */}
+                  </label>
+          			</div>
+          		</div>
+          	</div>
           </Col>
-          <Col size="md-4">
-
+          <Col size="md-9">
+            <div id="availability2">
+              <div className="panel panel-warning">
+               <div className="panel-heading">
+                 <h3 className="panel-title">Brews in process</h3>
+                 {/* <span className="pull-right clickable"><i className="glyphicon glyphicon-chevron-up"></i></span> */}
+               </div>
+               <div className="panel-body">Beer XYZ</div>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
