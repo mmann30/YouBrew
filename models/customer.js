2667b88f3f6ec6ed3971b3a463cd6de0a16843c4
mmann30 <michael.l.mann13@gmail.com>
2017-10-07 10:04:07 -0400

    converted references from client to customer

--- models/customer.js
+++ models/customer.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const clientSchema = new Schema({
+const customerSchema = new Schema({
   busName: { type: String, required: true, unique: true },
   custName: { type: String, required: true },  
   phone: { type: Number, required: true, unique: true }, 
   notes: String
 });
 
-const Client = mongoose.model("Client", clientSchema);
+const Customer = mongoose.model("Customer", customerSchema);
 
-module.exports = Client;
+module.exports = Customer;
