4ba1cc4dd5290cf5e0db44c6d9d7eb2b8a021d79
mmann30 <michael.l.mann13@gmail.com>
2017-10-12 10:07:50 -0400

    added new property to Recipe model to track inventory

--- models/recipe.js
+++ models/recipe.js
@@ -8,9 +8,10 @@ const recipeSchema = new Schema({
   desc: { type: String, required: true },
   brewTime: { type: Number, required: true },
   production: ["Year-Round", "Seasonal", "Limited", "Specialty"],
+  availVol: { type: Number, default: 0},
   notes: String
 });
 
-const Recipe = mongoose.model("Recipe", recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema, "Recipe");
 
 module.exports = Recipe;
