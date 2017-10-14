ba6e05a2d8a2c4eb99dc7348ec37c0fe5dcdbc9f
Florian Hutter <florianhutter@Florians-MacBook-Pro.local>
2017-10-13 22:31:51 -0400

    couple of updates

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -5,14 +5,11 @@ const NoMatch = () =>
   <Container fluid>
     <Row>
       <Col size="md-12">
-          <h1>404 Page Not Found</h1>
-          <h1>
-            <span role="img" aria-label="Face With Rolling Eyes Emoji">
-              🙄
-            </span>
-          </h1>
+          <h1>Error 404</h1>
+            <img className="img-responsive" src={require("../404img/pagenotfound.png")} alt="You screwed up!" /> 
       </Col>
     </Row>
   </Container>;
 
 export default NoMatch;
+ 
