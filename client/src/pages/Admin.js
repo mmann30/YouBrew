ebc130fe96de3c3d4cff56478a57b1301096bc86
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-10-16 13:54:22 -0400

    cleaned up a few bugs

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -6,8 +6,8 @@ import { EditBtn , AddRecipeBtn , AddUserBtn , AddBatchBtn } from "../components
 import ReactTable from 'react-table';
 import Modal from 'react-modal';
 import "react-table/react-table.css";
-import { ReactTableDefaults } from 'react-table'
-import Switch from 'react-toggle-switch'
+import { ReactTableDefaults } from 'react-table';
+import Switch from 'react-toggle-switch';
 
 // this is a react-table feature that allows us to override some defaults
 Object.assign(ReactTableDefaults, {
@@ -82,15 +82,15 @@ class Admin extends Component {
 
   render() {
     return (
-                  
+
 
 
       <Container>
 
                     <AddRecipeBtn onClick={this.openModal}>Add new recipe</AddRecipeBtn>
-                    <AddUserBtn onClick={this.openModal}>Add new user</AddRecipeBtn>
-                    <AddBatchBtn onClick={this.openModal}>Add recipe</AddRecipeBtn>
-                   
+                    <AddUserBtn onClick={this.openModal}>Add new user</AddUserBtn>
+                    <AddBatchBtn onClick={this.openModal}>Add recipe</AddBatchBtn>
+
         <Row>
           <Col size="md-10">
             <h1>Access Control</h1>
