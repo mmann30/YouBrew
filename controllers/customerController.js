99a6279c8315e10e2ba23a00fa924b0f6435ebff
mmann30 <michael.l.mann13@gmail.com>
2017-10-16 21:36:08 -0400

    state updates with customer info to pass to order modal

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -5,7 +5,7 @@ module.exports = {
   findAll: function(req, res) {
     db.Customer
       .find(req.query)
-      .sort({ busName: -1 })
+      .sort({ busName: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
