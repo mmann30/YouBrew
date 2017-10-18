dc8f8f00038b82c91f99d382e610e056dc66e977
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-10-17 21:12:06 -0400

    Change button colors

--- client/src/components/Buttons/EditBtn.js
+++ client/src/components/Buttons/EditBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const EditBtn = props =>
-  <button {...props} className="btn btn-danger">
+  <button {...props} style={{backgroundColor: "rgb(231,175,95)", border: "none"}} className="btn btn-danger">
     {props.children}
   </button>;
--- client/src/components/Buttons/OrderBtn.js
+++ client/src/components/Buttons/OrderBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const OrderBtn = props =>
-  <button {...props} style={{ float: "right" }} className="btn btn-success">
+  <button {...props} style={{ float: "right" }} className="btn btn-primary">
     {props.children}
   </button>;
